2e324c973fe9b077070f1597cfd6867aa94c0d6a
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2022-10-10 01:14:26 -0400

    added user routes, still need thoughts and to combine them all in index

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -22,7 +22,7 @@ const userController = {
   },
     // 
   // get one user by id
-getPizzaById({ params }, res) {
+getUsersById({ params }, res) {
     Pizza.findOne({ _id: params.id })
       .populate({
         path: 'Thoughts',
@@ -41,6 +41,43 @@ getPizzaById({ params }, res) {
         res.status(400).json(err);
       });
   },
+
+  // create new User
+createUser({ body }, res) {
+    User.create(body)
+      .then(dbuserData => res.json(dbuserData))
+      .catch(err => res.status(400).json(err));
+  },
+
+
+  // update user by id(PUT)
+updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+      .then(dbuserData => {
+        if (!dbuserData) {
+          res.status(404).json({ message: 'No User found with this id!' });
+          return;
+        }
+        res.json(dbuserData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
+
+
+  // delete user
+deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No User found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
+  }
+  
+
 
 
 }
---
+++ routes/api/user-routes.js
@@ -0,0 +1,27 @@
+const router = require('express').router();
+
+const {
+    getAllUsers,
+    getUsersById,
+    createUser,
+    updateUser,
+    deleteUser
+  } = require('../../controllers/user-controller');
+  
+  // Set up GET all and POST at /api/users
+router
+.route('/')
+.get(getAllUsers)
+.post(createUser);
+
+// Set up GET one, PUT, and DELETE at /api/users/:id
+router
+.route('/:id')
+.get(getUsersById)
+.put(updateUser)
+.delete(deleteUser);
+
+module.exports = router;
+
+
+
